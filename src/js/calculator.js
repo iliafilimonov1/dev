77e5afb3584bdc27ebdef16d434a2deccbcb66b0
Ilia Filimonov <9991571155i@gmail.com>
2023-02-18 20:16:44 +0700

    fix task

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,15 +1,5 @@
 export function sum(a, b) {
-                    return a + b;
-
-
-    
-
-
-
-
-
-                    
-                    
+    return a + b;
 }
 
 export function subtract(a, b) {
