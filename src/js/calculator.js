cae0a8e9d34b1dbe5a4b4f2c339ca3bafc64afe3
Ilia Filimonov <9991571155i@gmail.com>
2023-04-15 21:00:33 +0300

    added spaces. test ci/cd

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -11,11 +11,11 @@ export function sum(a, b) {
 }
 
 export function subtract(a, b) {
-    return a - b;
+            return a - b;
 }
 
 export function multiply(a, b) {
-    return a * b;
+            return a * b;
 }
 
 export function divide(a, b) {
