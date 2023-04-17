b4808f9fae4e23a951a7ea6356cbb71c2418ceb8
Ilia Filimonov <9991571155i@gmail.com>
2023-04-17 20:44:54 +0300

    added errors for calculator

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -4,14 +4,15 @@ export function sum(a, b) {
     
     
     
-    
-    
-    
-                            return a + b;
+         return a + b;
 }
 
 export function subtract(a, b) {
-            return a - b;
+            
+    
+    
+    
+    return a - b;
 }
 
 export function multiply(a, b) {
